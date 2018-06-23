cc3cb1e8d01e6ddfcaca932a6e2444253dcf427f
shock <github@wdoughty.net>
2018-06-23 07:35:44 -0500

    WOW

--- config/waterline.js
+++ config/waterline.js
@@ -12,14 +12,14 @@ waterline.registerModel(User);
 module.exports.initialize = function(app, PORT, callback) {
   // Initialize the whole database and store models and connections to app
   waterline.initialize(config, function(err, models) {
-      if(err){
-          console.log('error initializing waterline: '+err);
-          throw err;
-      }
-      console.log('waterline initialized');
+    if(err){
+      console.log('error initializing waterline: '+err);
+      throw err;
+    }
+    console.log('waterline initialized');
 
-      app.models = models.collections;
-      callback(models.collections, models.connections);
+    app.models = models.collections;
+    callback(models.collections, models.connections);
   });
 }
 
